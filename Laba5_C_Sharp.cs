4494119852392a2f27bb620689e133a2486855d9
AlexeyBilko <53230108+AlexeyBilko@users.noreply.github.com>
2020-11-25 11:28:41 +0200

    Add files via upload

---
+++ Laba5_C_Sharp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Варіант 22
+            int res;
+            int tmp;
+            for (int i = 1000000; i <= 1000000000; i++)
+            {
+                res = 0;
+                tmp = i;
+                while (tmp != 0)
+                {
+                    res = (res * 10) + (tmp % 10);
+                    tmp /= 10;
+                }
+                if (res == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
